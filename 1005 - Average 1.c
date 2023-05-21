c418377288051ec9787e66340a47ed1a4ec4bcb2
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-05-21 21:56:53 +0600

    Create 1005 - Average 1.c
    solved with c

---
+++ 1005 - Average 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+    double  a, b;
+    scanf ("%lf %lf",&a,&b);
+   double   MEDIA =  ((a*3.5)+(b*7.5))/ 11;
+
+    printf ("MEDIA = %.5lf\n",MEDIA);
+    return 0;
+}
