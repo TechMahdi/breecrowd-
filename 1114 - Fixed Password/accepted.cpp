116077972d8f5a8e8eca494e13c8123d53ecccdd
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-09-27 23:58:36 +0600

    Create accepted.cpp

---
+++ 1114 - Fixed Password/accepted.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n;
+    int i;
+i++;
+    for(;i>0 ; )
+    {
+        cin>>n;
+        if(n==2002)
+        {
+            cout<<"Acesso Permitido"<<"\n";
+            return 0;
+        }
+        else
+        {
+            cout<<"Senha Invalida"<<"\n";
+        }
+    }
+}
